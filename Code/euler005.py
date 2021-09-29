12a0d8d798c4051d11e72e8e3bc486b1674a5ca7
PratikPedhavi <pratikpdv@gmail.com>
2021-09-29 10:31:08 +0530

    smallest multiple

---
+++ Code/euler005.py
@@ -0,0 +1,16 @@
+# Project Euler 5: LCM
+# Dated: 2021-09-29
+
+import sys
+import math
+
+def smallest_multiple(N):
+    if N > 0:
+        return int((N*smallest_multiple(N-1))/math.gcd(N,smallest_multiple(N-1)))
+    else:
+        return 1
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(smallest_multiple(n))
