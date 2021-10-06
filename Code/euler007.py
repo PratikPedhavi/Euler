f49c8077c0e08d69315c64a91f8d6f401b4d0d0d
PratikPedhavi <pratikpdv@gmail.com>
2021-10-06 09:21:14 +0530

    10001st prime

---
+++ Code/euler007.py
@@ -0,0 +1,28 @@
+# Project Euler 7: 10001th prime
+# Dated: 2021-10-06
+
+import sys
+
+def sieveOfEratosthenes():
+    n = 1000005
+    primes = {}
+    prime = [True for i in range(n+1)]
+    p = 2
+    while p*p < n:
+        if prime[p]:
+            for k in range(p*p,n+1,p):
+                prime[k]=False
+
+        p+=1
+    i=1
+    for p in range(2,n+1):
+        if prime[p]:
+            primes[i] = p
+            i += 1
+    return primes
+
+t = int(input().strip())
+primes = sieveOfEratosthenes()
+for a0 in range(t):
+    n = int(input().strip())
+    print(primes[n-1])
